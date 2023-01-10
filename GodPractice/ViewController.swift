2f4818fcbbecd9ad26eceec6f191b47ab1a383a4
陳逸煌 <ehun9376@gmail.com>
2023-01-10 13:44:12 +0800

    init

---
+++ GodPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GodPractice
+//
+//  Created by 陳逸煌 on 2023/1/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
